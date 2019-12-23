dcb102f8038d6ad689884f1ab49fcd75cd315a85
y1m1ng1in <43800898+y1m1ng1in@users.noreply.github.com>
2019-12-22 22:15:25 -0800

    enqueue scripts in backend

--- yiming1-plugin/yiming1-plugin.php
+++ yiming1-plugin/yiming1-plugin.php
@@ -18,6 +18,10 @@ class Yiming1Plugin {
     add_action('init', array($this, 'custom_post_type'));
   }
 
+  function register(){
+    add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+  }
+
   function activate(){
     // generate a CPT
     $this->custom_post_type();
@@ -38,10 +42,17 @@ class Yiming1Plugin {
   function custom_post_type(){
     register_post_type('event', ['public' => true, 'label' => 'Events']);
   }
+
+  function enqueue(){
+    // enqueue all the scripts
+    wp_enqueue_style('my plugin style', plugins_url('/assets/style.css', __FILE__));
+    wp_enqueue_script('my plugin script', plugins_url('/assets/myscript.js', __FILE__));
+  }
 }
 
 if(class_exists('Yiming1Plugin')){
   $yiming1Plugin = new Yiming1Plugin();
+  $yiming1Plugin->register();
 }
 
 register_activation_hook(__FILE__, array($yiming1Plugin, 'activate'));
