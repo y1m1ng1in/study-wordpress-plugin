de0fc425b7be1d51b83f7ea60b272179cb0aef3e
y1m1ng1in <43800898+y1m1ng1in@users.noreply.github.com>
2019-12-28 22:48:54 -0800

    add admin subpages

--- yiming1-plugin/includes/Api/SettingsApi.php
+++ yiming1-plugin/includes/Api/SettingsApi.php
@@ -4,6 +4,8 @@ namespace Includes\Api;
 class SettingsApi {
   public $admin_pages = array();
 
+  public $subpages = array();
+
   public function register(){
     if(!empty($this->admin_pages)){
       add_action('admin_menu', array($this, 'add_admin_menu'));
@@ -15,10 +17,48 @@ class SettingsApi {
     return $this;
   }
 
+  public function with_subpage(string $title=null){
+    if(empty($this->admin_pages)){
+      return $this; 
+    }
+    $admin_page = $this->admin_pages[0];
+    $pages = [
+      [
+        'parent_slug' => $admin_page['menu_slug'], 
+        'page_title' => $admin_page['page_title'],
+        'menu_title' => ($title) ? $title : $admin_page['menu_title'], 
+        'capability' => $admin_page['capability'], 
+        'menu_slug' => $admin_page['menu_slug'], 
+        'callback' => $admin_page['callback']
+      ], # subpages don't have icon url and position
+    ];
+    $this->subpages = $pages;
+    return $this;
+  }
+
+  public function add_subpages(array $pages){
+    $this->subpages = array_merge($this->subpages, $pages);
+    return $this; 
+  }
+
   public function add_admin_menu(){
     foreach($this->admin_pages as $page){
-      add_menu_page($page['page_title'], $page['menu_title'], $page['capability'],
-        $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position']);
+      add_menu_page($page['page_title'], 
+                    $page['menu_title'], 
+                    $page['capability'],
+                    $page['menu_slug'], 
+                    $page['callback'], 
+                    $page['icon_url'], 
+                    $page['position']);
+    }
+
+    foreach($this->subpages as $page){
+      add_submenu_page($page['parent_slug'],
+                       $page['page_title'], 
+                       $page['menu_title'], 
+                       $page['capability'],
+                       $page['menu_slug'], 
+                       $page['callback']);
     }
   }
 }
--- yiming1-plugin/includes/Pages/Admin.php
+++ yiming1-plugin/includes/Pages/Admin.php
@@ -10,22 +10,58 @@ use \Includes\Api\SettingsApi;
 class Admin extends BaseController {
   public $settings;
 
+  public $subpages;
+
+  public $admin_pages;
+
   public function __construct(){
     $this->settings = new SettingsApi();
-  }
 
-  public function register(){
-    $pages = [
+    $this->admin_pages = [
       [
         'page_title' => 'Yiming1 Plugin', 
         'menu_title' => 'Yiming', 
         'capability' => 'manage_options', 
         'menu_slug' => 'yiming1_plugin', 
-        'callback' => function(){echo '<h1>p</h1>';}, 
+        'callback' => function(){echo '<h1>Admin Page 1</h1>';}, 
         'icon_url' => 'dashicons-store', 
         'position' => 110
       ]
     ];
-    $this->settings->add_pages($pages)->register();
+
+    $this->subpages = [
+      [
+        'parent_slug' => 'yiming1_plugin', 
+        'page_title' => 'Custom Post Types',
+        'menu_title' => 'CPTs', 
+        'capability' => 'manage_options', 
+        'menu_slug' => 'yiming1_plugin_cpt', 
+        'callback' => function(){echo '<h1>Custom Post Type Manager</h1>';}
+      ], # subpages don't have icon url and position
+      [
+        'parent_slug' => 'yiming1_plugin', 
+        'page_title' => 'Custom Taxonomies',
+        'menu_title' => 'Taxonomies', 
+        'capability' => 'manage_options', 
+        'menu_slug' => 'yiming1_plugin_taxonomies', 
+        'callback' => function(){echo '<h1>Custom Taxonomies</h1>';}
+      ], # subpages don't have icon url and position
+      [
+        'parent_slug' => 'yiming1_plugin', 
+        'page_title' => 'Custom Widgets',
+        'menu_title' => 'Widgets', 
+        'capability' => 'manage_options', 
+        'menu_slug' => 'yiming1_plugin_widgets', 
+        'callback' => function(){echo '<h1>Custom Widgets</h1>';}
+      ], # subpages don't have icon url and position
+    ];
+  }
+
+  public function register(){
+    $this->settings
+         ->add_pages($this->admin_pages)
+         ->with_subpage('Dashboard')
+         ->add_subpages($this->subpages)
+         ->register();
   }
 }
