ce53df69cdc8697a6f9485ef30080d17c9faa1eb
y1m1ng1in <43800898+y1m1ng1in@users.noreply.github.com>
2019-12-29 00:15:34 -0800

    custom fields in admin page

--- yiming1-plugin/includes/Api/SettingsApi.php
+++ yiming1-plugin/includes/Api/SettingsApi.php
@@ -6,6 +6,12 @@ class SettingsApi {
 
   public $subpages = array();
 
+  public $settings = array();
+
+  public $sections = array(); 
+
+  public $fields = array();
+
   public function register(){
     if(!empty($this->admin_pages)){
       add_action('admin_menu', array($this, 'add_admin_menu'));
@@ -59,6 +65,48 @@ class SettingsApi {
                        $page['capability'],
                        $page['menu_slug'], 
                        $page['callback']);
+    }
+  }
+
+  public function set_settings(array $settings){
+    $this->settings = $settings;
+    return $this;
+  }
+
+  public function set_sections(array $sections){
+    $this->sections = $sections;
+    return $this;
+  }
+
+  public function set_fields(array $fields){
+    $this->fields = $fields;
+    return $this;
+  }
+
+  public function register_custom_fields(){
+    // register setting
+    foreach($this->settings as $setting){
+      register_setting($setting['option_group'], 
+                       $setting['option_name'], 
+                      (isset($setting['callback'])) ? $setting['callback'] : '');
+    }
+
+    // add setting section
+    foreach($this->sections as $section){
+      add_settings_section($section['id'], 
+                           $section['title'], 
+                           (isset($section['callback'])) ? $section['callback'] : '', 
+                           $section['page']);
+    }
+
+    // add setting field
+    foreach($this->fields as $field){
+      add_settings_field($field['id'], 
+                         $field['title'], 
+                         (isset($field['callback'])) ? $field['callback'] : '', 
+                         $field['page'], 
+                         $field['section'], 
+                         (isset($field['args'])) ? $field['args'] : '');
     }
   }
 }
