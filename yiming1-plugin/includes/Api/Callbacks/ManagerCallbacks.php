b1e85bddf83c5d29d9288b38a1d8b8accbd274e5
y1m1ng1in <43800898+y1m1ng1in@users.noreply.github.com>
2020-01-09 07:14:33 -0800

    toggle ui checkbox

--- yiming1-plugin/includes/Api/Callbacks/ManagerCallbacks.php
+++ yiming1-plugin/includes/Api/Callbacks/ManagerCallbacks.php
@@ -18,7 +18,9 @@ class ManagerCallbacks extends BaseController {
     $name = $args['label_for'];
     $class = $args['class'];
     $checkbox = get_option($name);
-    echo '<input type="checkbox" name="' . $name . '" value="1" class="' 
-      . $classes . '" ' . ($checkbox ? 'checked' : '') . '>';
+    echo '<div class="' . $class . '"><input type="checkbox"  id="'
+      . $name .'" name="' . $name . '" value="1" class="'. $class .'"' 
+      . ($checkbox ? 'checked' : '') . '><label for="'
+      . $name .'"><div></div></label></div>';
   }
 }
